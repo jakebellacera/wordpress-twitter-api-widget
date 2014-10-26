7e852c3292b417ad3d0fa716932761db3ffc61b2
Jake Bellacera <hi@jakebellacera.com>
2014-10-25 22:23:19 -0700

    Adding initial widget code

---
+++ twitter-api-widget.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Twitter API Widget
+ * Plugin URI: https://github.com/jakebellacera/wordpress-twitter-api-widget
+ * Description: A simple widget for Wordpress that displays your Twitter feed through the Twitter API.
+ * Version: 1.0.0
+ * Author: Jake Bellacera
+ * Author URI: http://jakebellacera.com/
+ * License: MIT
+ */
+
+defined('ABSPATH') or die();
